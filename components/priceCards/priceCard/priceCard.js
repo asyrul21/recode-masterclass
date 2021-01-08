6d23f2f5d8429a45d3755104a11729f10c4023a7
asyrul21 <asyrulhafetzy.21@gmail.com>
2021-01-08 22:05:42 +0800

    discounted price in price card

--- components/priceCards/priceCard/priceCard.js
+++ components/priceCards/priceCard/priceCard.js
@@ -1,69 +1,104 @@
 // template to create a new component
-import React from "react"
-import propTypes from "prop-types"
-import classnames from "classnames"
+import React, { createContext } from "react";
+import propTypes from "prop-types";
+import classnames from "classnames";
 
 // import component
-import Button from "components/buttons/indexButton/indexButton"
+import Button from "components/buttons/indexButton/indexButton";
 
 // import style
-import styles from "./priceCard.module.scss"
+import styles from "./priceCard.module.scss";
 
-export const PriceCard = props => {
-  const { heading, subHeading, text, price, buttonText, buttonLink, className } = props
+export const PriceCard = (props) => {
+  const {
+    heading,
+    subHeading,
+    text,
+    price,
+    discountPrice,
+    buttonText,
+    buttonLink,
+    className,
+  } = props;
 
   // define container classes here
   const styleClasses = classnames({
     [styles.priceCardContainer]: true,
     [`${className}`]: className,
-  })
+  });
 
   return (
     <div className={styleClasses}>
       {/* whatever you need to do here */}
       {/* avoid using style tags */}
-        <p className={styles.priceCard_heading}>{ heading }</p>
-        <p className={styles.priceCard_subHeading}>{ subHeading }</p>
-        <div className={styles.priceCard_text}>
-            <p>{ text }</p>
-        </div>
-        <p className={styles.priceCard_price}>{ price }</p>
-        <Button text={buttonText} backgroundColor="navy-blue" fontColor="white"
-        className={styles.priceCard_button} external link={buttonLink}/>
+      <div>
+        <p className={styles.priceCard_heading}>{heading}</p>
+        <p className={styles.priceCard_subHeading}>{subHeading}</p>
+      </div>
+
+      <div className={styles.priceCard_text}>
+        <p>{text}</p>
+      </div>
+      <div>
+        {discountPrice && (
+          <>
+            <p className={styles.priceCard_disocuntprice}>{discountPrice}</p>
+            <p
+              className={classnames(
+                styles.priceCard_price,
+                styles.priceCard_price_stroke
+              )}
+            >
+              {price}
+            </p>
+          </>
+        )}
+        {!discountPrice && <p className={styles.priceCard_price}>{price}</p>}
+      </div>
+      <div>
+        <Button
+          text={buttonText}
+          backgroundColor="navy-blue"
+          fontColor="white"
+          className={styles.priceCard_button}
+          external
+          link={buttonLink}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 /* Prop Definition */
 PriceCard.propTypes = {
-    /*
-    *
-    * heading of the card */
-    heading: propTypes.string,
-    /*
-    *
-    * subHeading of the card */
-    subHeading: propTypes.string,
-    /*
-    *
-    * some text */
-    text: propTypes.string,
-    /*
-    *
-    * the price */
-    price: propTypes.string,
-    /*
-    *
-    * the text on the button */
-    buttonText: propTypes.string,
-    /*
-    *
-    * where the buttons leads to */
-   buttonLink: propTypes.string,
-    /*
-    *
-    * any applied classnames */
-    className: propTypes.string,
-}
+  /*
+   *
+   * heading of the card */
+  heading: propTypes.string,
+  /*
+   *
+   * subHeading of the card */
+  subHeading: propTypes.string,
+  /*
+   *
+   * some text */
+  text: propTypes.string,
+  /*
+   *
+   * the price */
+  price: propTypes.string,
+  /*
+   *
+   * the text on the button */
+  buttonText: propTypes.string,
+  /*
+   *
+   * where the buttons leads to */
+  buttonLink: propTypes.string,
+  /*
+   *
+   * any applied classnames */
+  className: propTypes.string,
+};
 
-export default PriceCard
+export default PriceCard;
--- components/priceCards/priceCards.js
+++ components/priceCards/priceCards.js
@@ -1,51 +1,53 @@
 // template to create a new component
-import React from "react"
-import propTypes from "prop-types"
-import classnames from "classnames"
+import React from "react";
+import propTypes from "prop-types";
+import classnames from "classnames";
 
 // dependency component
-import PriceCard from "./priceCard/priceCard"
-
+import PriceCard from "./priceCard/priceCard";
 
 // import style
-import styles from "./priceCards.module.scss"
+import styles from "./priceCards.module.scss";
 
-export const PriceCards = props => {
-  const { data, className } = props
+export const PriceCards = (props) => {
+  const { data, className } = props;
 
   // define container classes here
   const styleClasses = classnames({
     [styles.priceCardsContainer]: true,
     [`${className}`]: className,
-  })
+  });
 
   return (
     <div className={styleClasses}>
-      {data.map((pricing, idx)=> {
-          return (
-            <PriceCard key={idx} {...pricing}/>
-          )
+      {data.map((pricing, idx) => {
+        console.log("pricing");
+        console.log(pricing);
+        return <PriceCard key={idx} {...pricing} />;
       })}
     </div>
-  )
-}
+  );
+};
 
 /* Prop Definition */
 PriceCards.propTypes = {
   /*
    *
    * the array of pricing information */
-  data: propTypes.arrayOf(propTypes.shape({
+  data: propTypes.arrayOf(
+    propTypes.shape({
       heading: propTypes.string,
       subHeading: propTypes.string,
       text: propTypes.string,
       price: propTypes.string,
-      buttonText: propTypes.string
-  })),
+      discountPrice: propTypes.string,
+      buttonText: propTypes.string,
+    })
+  ),
   /*
    *
    * any applied classnames */
   className: propTypes.string,
-}
+};
 
-export default PriceCards
+export default PriceCards;
